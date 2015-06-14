6c0e374055456f4073ac65551bb24c6f81132595
Arthur Skobara <skobara.arthur@gmail.com>
2015-06-14 21:33:07 +0600

    respect .mailmap

---
+++ src/mailmap.rs
@@ -0,0 +1,116 @@
+use std::path::Path;
+use std::fmt;
+use std::io::{self,BufReader,BufRead};
+use std::fs::File;
+use std::collections::HashMap;
+use std::string;
+use git2;
+
+macro_rules! ostring {
+    ($e:expr) => (match $e {
+        Some(s) => Some(String::from_str(s)),
+        None => None
+    })
+}
+
+struct Author {
+    name: Option<String>,
+    email: Option<String>,
+    namemap: HashMap<String, Author>
+}
+
+impl Author {
+    pub fn new(name: Option<String>, email: Option<String>) -> Author {
+        Author {
+            name: name,
+            email: email,
+            namemap: HashMap::new()
+        }
+    }
+
+    pub fn set_name(&mut self, name: Option<String>) -> &mut Author {
+        self.name = name;
+        self
+    }
+
+    pub fn set_email(&mut self, email: Option<String>) -> &mut Author {
+        self.email = email;
+        self
+    }
+
+    pub fn namemap_insert(&mut self, name: String, author: Author) -> &mut Author {
+        self.namemap.insert(name, author);
+        self
+    }
+}
+
+impl fmt::Display for Author {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?} <{:?}>", self.name, self.email)
+    }
+}
+
+impl fmt::Debug for Author {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?} <{:?}> {:?}", self.name, self.email, self.namemap)
+    }
+}
+
+pub struct Mailmap {
+    items: HashMap<String, Author>
+}
+
+impl Mailmap {
+
+    pub fn new(path: &Path) -> Result<Mailmap, io::Error> {
+        let file = try!(File::open(path));
+        let reader = BufReader::new(file);
+
+        // for more help with this regex see https://www.debuggex.com/r/eF5E6HQm4aAhXEtN
+        let re = regex!(r"^((?P<new_name>.+?)\s+)??<\s*(?P<new_email>.+?)\s*>((\s+(?P<old_name>.+?))??\s+<\s*(?P<old_email>.+?)\s*>)?");
+
+        let mut authors: HashMap<String, Author> = HashMap::new();
+
+        for line in reader.lines() {
+            let line = try!(line);
+            if line.chars().nth(0) == Some('#') { continue; }
+
+            if let Some(caps) = re.captures(&line[..]) {
+                let mut old_email = ostring!(caps.name("old_email"));
+                let old_name  = ostring!(caps.name("old_name"));
+                let mut new_email = ostring!(caps.name("new_email"));
+                let new_name  = ostring!(caps.name("new_name"));
+
+                if old_email.is_none() {
+                    old_email = new_email.clone();
+                    new_email = None;
+                }
+
+                let mut me = authors.entry(old_email.unwrap()).or_insert(Author::new(None, None));
+
+                if let Some(name) = old_name {
+                    me.namemap_insert(name, Author::new(new_name, new_email));
+                } else {
+                    me.set_name(new_name).set_email(new_email);
+                }
+            }
+        }
+
+        Ok(Mailmap {
+            items: authors
+        })
+    }
+
+    pub fn map_user(&self, signature: &git2::Signature) -> Result<String, string::FromUtf8Error> {
+        let name =  ostring!(signature.name()).expect("Valid utf8 string");
+        let email = ostring!(signature.email()).expect("Valid utf8 string");
+        let author = match self.items.get(&email) {
+            Some(item) => if item.namemap.is_empty() { Some(item) } else { item.namemap.get(&name) },
+            None => None
+        };
+
+        Ok(author.map_or_else(|| format!("{}", signature), |item| format!("{} <{}>", item.name.clone().unwrap_or(name), item.email.clone().unwrap_or(email))))
+    }
+
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 #![feature(core)]
 #![feature(collections)]
+#![feature(plugin)]
+
+#![plugin(regex_macros)]
 
 extern crate git2;
 extern crate chrono;
 extern crate getopts;
-// extern crate unicode;
 extern crate docopt;
 extern crate rustc_serialize;
 extern crate collections;
 extern crate core;
+extern crate regex;
 
 use docopt::Docopt;
 
 mod snapshot;
 mod heatmap;
+mod mailmap;
 
 #[derive(RustcDecodable)]
 pub struct Args {
@@ -37,26 +41,53 @@ Options:
     }
 }
 
+macro_rules! error(
+    ($($arg:tt)*) => (
+        use std::io::Write;
+        match writeln!(&mut ::std::io::stderr(), $($arg)* ) {
+            Ok(_) => {},
+            Err(x) => panic!("Unable to write to stderr: {}", x),
+        }
+    )
+);
+
+#[macro_export]
+/// converts errors into None
+macro_rules! otry {
+    ($e:expr) => (match $e {
+        Ok(e) => e,
+        Err(e) => {
+            error!("ERROR!: {:?} {} {}", e, file!(), line!());
+            return None
+        }
+    })
+}
+
 mod gitostat {
 
     use git2::{self, Diff};
     use std::collections::HashMap;
     use std::path::Path;
+    use std::error::Error;
     use Args;
 
     use snapshot::Snapshot;
     use heatmap::Heatmap;
+    use mailmap::Mailmap;
 
     pub fn run(args: &Args) -> Result<(), git2::Error> {
         let path = Path::new(&args.arg_path);
         let repo = try!(git2::Repository::open(path));
 
-        try!(self::info(&repo));
+        let mailmap = try!(Mailmap::new(&path.join(".mailmap"))
+                           .map_err(|err| git2::Error::from_str(err.description())));
+
+        try!(self::info(&repo, &mailmap));
 
         Ok(())
     }
 
-    fn info(repo: &git2::Repository) -> Result<(), git2::Error> {
+    fn info(repo: &git2::Repository, mailmap: &Mailmap) -> Result<(), git2::Error> {
         let mut heatmap = Heatmap::new();
         let mut authors: HashMap<String, usize> = HashMap::new();
         let mut revwalk = try!(repo.revwalk());
@@ -67,31 +98,33 @@ mod gitostat {
 
         let commits = revwalk.filter_map(|oid| {
             // trying lookup commit in repo, skip if any error
-            let commit = match repo.find_commit(oid) {
-                Ok(commit) => commit,
-                Err(_) => { return None; }
-            };
+            let commit = otry!(repo.find_commit(oid));
 
             // also skip merge-commits
             if commit.parents().len() > 1 { return None; }
 
             Some(commit)
-        }).take(10);
+        });
 
         // println!("total count: {}", oids.len());
 
         for commit in commits {
-            let parent = try!(commit.parent(0));
             let tree = try!(commit.tree());
-            let ptree = try!(parent.tree());
-            let diff = try!(Diff::tree_to_tree(&repo, Some(&ptree), Some(&tree), None));
+            let ptree = if commit.parents().len() == 1 {
+                let parent = try!(commit.parent(0));
+                Some(try!(parent.tree()))
+            } else {
+                None
+            };
+
+            let diff = try!(Diff::tree_to_tree(&repo, ptree.as_ref(), Some(&tree), None));
             let stats = try!(diff.stats());
 
             heatmap.append(&commit.time());
 
             let files = try!(Snapshot::new(&repo, &commit));
 
-            let uniq_name: String = get_uniq_name(&commit.author());
+            let uniq_name: String = try!(mailmap.map_user(&commit.author()).map_err(|err| git2::Error::from_str(err.description())));
             *authors.entry(uniq_name).or_insert(0) += 1;
 
             println!("{} {}", commit.id(), commit.author());
@@ -116,10 +149,6 @@ mod gitostat {
         }
 
         Ok(())
-    }
-
-    fn get_uniq_name(author: &git2::Signature) -> String {
-        format!("{} <{}>", author.name().unwrap(), author.email().unwrap())
     }
 
 }
